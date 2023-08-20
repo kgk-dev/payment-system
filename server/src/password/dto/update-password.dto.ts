ad0b3d8a7e7e3a6510a005b9f428ff3d1e6ac94e
Kaung Min Htet <kgminhtet000@gmail.com>
2023-08-20 21:31:20 +0630

    Deleted Server Bacause Nestjs is complicated

--- server/src/password/dto/update-password.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePasswordDto } from './create-password.dto';
-
-export class UpdatePasswordDto extends PartialType(CreatePasswordDto) {}
--- server/src/password/password.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PasswordService } from './password.service';
-import { PasswordController } from './password.controller';
-
-@Module({
-  controllers: [PasswordController],
-  providers: [PasswordService],
-})
-export class PasswordModule {}
