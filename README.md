6f600b2e164e6d72d53a0907a95e0f7ba3f85340
Kaung Min Htet <kgminhtet000@gmail.com>
2023-10-14 09:59:24 +0630

    Change VerifyOTP and VerifyPassword

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # payment-system
+
+This project is implemented with React, Node, Express and Postgres. ORM is used Prisma. 
+
+In server, you need to create .env file.In this file, you must specify DATABASE_URL, LOGIN_TOKEN, ADMIN_LOGIN_TOKEN and SIGNUP_TOKEN. Token are random hexidecimal values. 
+
+Then, apply yarn install to client and server. Done.
--- server/src/middlewares/auth/verifyOTP.js
+++ server/src/middlewares/auth/verifyOTP.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken')
 module.exports = async (req, res, next) => {
   console.log("cookies: ", req.cookies)
   const token = req.cookies.token
-  console.log("[OTP] token: ", token)
   const secret = req.body['otp']
   if (token == null) return res.status(401).send('Unauthorized')
   jwt.verify(token, secret, (err, payload) => {
